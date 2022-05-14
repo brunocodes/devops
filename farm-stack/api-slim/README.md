dbcbc4ef3fd72bddb0cab8b082c12bd4fb512cc2
Bruno Batista <bho360@hotmail.com>
2022-05-14 18:49:32 -0400

    Added api-slim and python venv info

---
+++ farm-stack/api-slim/README.md
@@ -0,0 +1,13 @@
+# FastAPI MongoDB boilerplate
+* FastAPI
+* MongoDB motor
+
+### initial installation
+pip install "fastapi[all]"
+pip install "passlib[bcrypt]"
+pip install pytest
+pip install "python-jose[cryptography]"
+pip install httptools
+pip install uvloop
+pip install gunicorn
+pip install motor
--- farm-stack/api-slim/app/models.py
+++ farm-stack/api-slim/app/models.py
@@ -1,17 +1,8 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql.expression import text
-from sqlalchemy.sql.sqltypes import TIMESTAMP
-from sqlalchemy.dialects.postgresql import UUID
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
 
-from .database import Base
-
-class User(Base):
-    __tablename__ = "users"
-    
-    id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
-    user_name = Column(String, nullable=False)
-    email = Column(String, nullable=False, unique=True)
-    password = Column(String, nullable=False)
-    api_key = Column(UUID(as_uuid=True), nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
