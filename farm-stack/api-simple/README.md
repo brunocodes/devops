22603686fadff005a5fa02ad5fbe691a72e2d309
Bruno Batista <bho360@hotmail.com>
2022-05-16 18:22:56 -0300

    Deleted api-simple

--- farm-stack/api-simple/README.md
+++
@@ -1,9 +0,0 @@
-# FastAPI MongoDB boilerplate
-* FastAPI
-* MongoDB motor
-
-### initial installation
-pip install "fastapi[all]"
-pip install gunicorn
-pip install motor
-pip install pytest
--- farm-stack/api-simple/app/database.py
+++
@@ -1,6 +0,0 @@
-import motor.motor_asyncio
-# from models import User
-
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/')
-database = client.dev_test
-user_collection = database.user
